e0ee933791c52a86b8c255ecd0ff78e5a9565e88
AngelaRecchia <79789965+AngelaRecchia@users.noreply.github.com>
2021-06-29 10:54:55 +0200

    view chat dinamica

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const app = new Vue({
     el: "#app",
-    data: { 
+    data: {
+
         contacts: [
             {
                 name: 'Michele',
@@ -45,7 +46,52 @@ const app = new Vue({
                         status: 'sent'
                     }
                 ],
+            },
+            {
+                name: 'Samuele',
+                avatar: '_3',
+                visible: false,
+                messages: [
+                    {
+                        date: '28/03/2020 10:10:40',
+                        text: 'La Marianna va in campagna',
+                        status: 'received'
+                    },
+                    {
+                        date: '28/03/2020 10:20:10',
+                        text: 'Sicuro di non aver sbagliato chat?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '28/03/2020 16:15:22',
+                        text: 'Ah scusa!',
+                        status: 'received'
+                    }
+                ],
+            },
+            {
+                name: 'Luisa',
+                avatar: '_4',
+                visible: false,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Lo sai che ha aperto una nuova pizzeria?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        text: 'Si, ma preferirei andare al cinema',
+                        status: 'received'
+                    }
+                ]
             }
         ]
+    },
+    methods: {
+        openChat(index) {
+            this.contacts.map(contact => contact.visible = false);
+            this.contacts[index].visible = true;
+        }
     }
 });
