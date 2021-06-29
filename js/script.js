ad8a1e8e0100ffdeb963a119657552089c27ef46
AngelaRecchia <79789965+AngelaRecchia@users.noreply.github.com>
2021-06-29 14:53:10 +0200

    box on msg

--- js/script.js
+++ js/script.js
@@ -9,21 +9,25 @@ const app = new Vue({
                 avatar: '_1',
                 visible: true,
                 searchShow: true,
+                ultimoAccesso: '11:15',
                 messages: [
                     {
                         date: '10/01/2020 15:30:55',
                         text: 'Hai portato a spasso il cane?',
-                        status: 'sent'
+                        status: 'sent',
+                        showInfoBox: false
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         text: 'Ricordati di dargli da mangiare',
-                        status: 'sent'
+                        status: 'sent',
+                        showInfoBox: false
                     },
                     {
                         date: '10/01/2020 16:15:22',
                         text: 'Tutto fatto!',
-                        status: 'received'
+                        status: 'received',
+                        showInfoBox: false
                     }
                 ],
             },
@@ -32,21 +36,25 @@ const app = new Vue({
                 avatar: '_2',
                 visible: false,
                 searchShow: true,
+                ultimoAccesso: '10:12',
                 messages: [
                     {
                         date: '20/03/2020 16:30:00',
                         text: 'Ciao come stai?',
-                        status: 'sent'
+                        status: 'sent',
+                        showInfoBox: false
                     },
                     {
                         date: '20/03/2020 16:30:55',
                         text: 'Bene grazie! Stasera ci vediamo?',
-                        status: 'received'
+                        status: 'received',
+                        showInfoBox: false
                     },
                     {
                         date: '20/03/2020 16:35:00',
                         text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                        status: 'sent'
+                        status: 'sent',
+                        showInfoBox: false
                     }
                 ],
             },
@@ -55,21 +63,25 @@ const app = new Vue({
                 avatar: '_3',
                 visible: false,
                 searchShow: true,
+                ultimoAccesso: '12:23',
                 messages: [
                     {
                         date: '28/03/2020 10:10:40',
                         text: 'La Marianna va in campagna',
-                        status: 'received'
+                        status: 'received',
+                        showInfoBox: false
                     },
                     {
                         date: '28/03/2020 10:20:10',
                         text: 'Sicuro di non aver sbagliato chat?',
-                        status: 'sent'
+                        status: 'sent',
+                        showInfoBox: false
                     },
                     {
                         date: '28/03/2020 16:15:22',
                         text: 'Ah scusa!',
-                        status: 'received'
+                        status: 'received',
+                        showInfoBox: false
                     }
                 ],
             },
@@ -78,16 +90,19 @@ const app = new Vue({
                 avatar: '_4',
                 visible: false,
                 searchShow: true,
+                ultimoAccesso: '11:19',
                 messages: [
                     {
                         date: '10/01/2020 15:30:55',
                         text: 'Lo sai che ha aperto una nuova pizzeria?',
-                        status: 'sent'
+                        status: 'sent',
+                        showInfoBox: false
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         text: 'Si, ma preferirei andare al cinema',
-                        status: 'received'
+                        status: 'received',
+                        showInfoBox: false
                     }
                 ]
             }
@@ -106,7 +121,8 @@ const app = new Vue({
                     const objMsg = {
                         date: now,
                         text: this.addMsg,
-                        status: 'sent'
+                        status: 'sent',
+                        showInfoBox: false
                     }
 
                     contact.messages.push(objMsg);
@@ -116,7 +132,8 @@ const app = new Vue({
                         const objMsgBack = {
                             date: dayjs().format('DD/MM/YYYY HH:mm:ss'),
                             text: "ok",
-                            status: 'received'
+                            status: 'received',
+                            showInfoBox: false
                         }
 
                         contact.messages.push(objMsgBack);
@@ -130,6 +147,16 @@ const app = new Vue({
 
         filterSearch() {
             this.contacts.forEach(contact => contact.searchShow = contact.name.toLowerCase().includes(this.toSearch.toLowerCase()));
+        },
+
+        showInfo(index, msgIndex) {
+            this.contacts.forEach((contact, i) => {
+                contact.messages.forEach((message, iM) => {
+                    if(i == index && iM == msgIndex) message.showInfoBox = !message.showInfoBox;
+                    else message.showInfoBox = false;
+                })
+            });
         }
+
     }
 });
